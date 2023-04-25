f2933532d3b9ff2dae98824468a834b236fe7f5c
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-04-25 17:06:11 -0300

    resources champions com parâmetro :name

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  resources :champions, except: [:show]
-  get 'champions/:name', to: 'champions#show', as: 'champion_by_name'
+  resources :champions, param: :name
 end
